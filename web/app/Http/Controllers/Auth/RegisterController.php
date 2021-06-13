58e000c5f8c652baaaa9666cec2c0d6ceb5417c0
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2021-06-13 17:24:46 +0900

    RegisterController

--- web/app/Http/Controllers/Auth/RegisterController.php
+++ web/app/Http/Controllers/Auth/RegisterController.php
@@ -9,3 +9,62 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+
+class RegisterController extends Controller
+{
+  /**
+   *  Send Register Link Email
+   *  送られてきた内容をテーブルに保存して認証メールを送信
+   *  @param Request $request
+   *  @return RegisterUser
+   *
+   */
+   public function sendMail(Request $request)
+   {
+       $this->validator($request->all())->validate();
+       $token = $this->createToken();
+       RegisterUser::destroy($request->email);
+       $passwordReset = new RegisterUser($request->all());
+       $passwordReset->token = $token;
+       $passwordReset->password = Hash::make($request->password);
+       $passwordReset->save();
+       $this->sendVerificationMail($passwordReset->email, $token);
+       return $passwordReset;
+   }
+
+   /**
+    * validator
+    * @param array $data
+    * @return \Illuminate\Contracts\Validation\Validator
+    */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name'  => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * create activation token
+     * トークンを作成する
+     * @return string
+     */
+    private function createToken()
+    {
+        return hash_hmac('sha256', Str::random(40), config('app.key'));
+    }
+
+    /**
+     * send verification mail
+     * メール送信
+     * @param string $email
+     * @param string $token
+     * @return void
+     */
+    private function sendVerificationMail($email, $token)
+    {
+        Mail::to($email)->send(new VerificationMail($token));
+    }
+}
